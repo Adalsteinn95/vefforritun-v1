6f38852defb6a8e94a4a9cd0369c357c9058ee89
Adalsteinn95 <adalsteinn95@gmail.com>
2018-01-19 14:55:01 +0000

    backend

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const routes = require('./articles');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+/*views*/
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.use('/',routes);
--- articles.js
+++ articles.js
@@ -1,1 +1,61 @@
 /* útfæra greina virkni */
+const express = require('express');
+const articles = express.Router();
+const marked = require('marked');
+const fs = require('fs');
+const fm = require('front-matter');
+
+/*util to read the files*/
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+/*files*/
+const encode = 'utf-8';
+const batman = './articles/batman-ipsum.md';
+const corporate = './articles/corporate-ipsum.md';
+const deloren = './articles/deloren-ipsum.md';
+const lorem = './articles/lorem-ipsum.md';
+
+
+/* data*/
+let data;
+
+
+/*read function */
+function read(file) {
+  readFileAsync(file)
+    .then((data) => {
+      writedata(fm(data.toString(encode)));
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function writedata(incoming){
+  //console.log(incoming);
+  data = incoming;
+  console.log(data);
+}
+
+
+function readdata() {
+  read(batman);
+  read(corporate);
+  read(deloren);
+  read(lorem);
+
+};
+
+readdata();
+
+articles.get('/', (req, res) => {
+
+
+  res.render('index', {
+    title: 'hallo this is awesome'
+  });
+});
+
+module.exports = articles;
